c2e28f84b5bf604dc09bf22d92a94a6c69d769c9
LeonKar <l.karagishev@gmail.com>
2022-02-28 10:03:28 +0100

    Login

--- F1 Stats/src/Main.java
+++ F1 Stats/src/Main.java
@@ -1,6 +1,27 @@
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Estadistica F1");
+		String login="", password="", check;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduzca su login:");
+		login = sc.next();
+		System.out.println("Introduzca su contraseña:");
+		password = sc.next();
+		check = login(login, password);
+		if(check.equals("error")) {
+			System.out.println("Login o contraseña incorrecto");
+		}
+	}
+	
+	public static String login(String login, String password) {
+		String check="error";
+		if(login.equals("admin")&&password.equals("1234")) {
+			System.out.println("Hola admin");
+		}else if(login.equals("user1")&&password.equals("password1")){
+			System.out.println("Hola " + login);
+		}
+		return check;
 	}
 }
