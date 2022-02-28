a65a7f9abd1c0a497bd1c332c4b1d262cbb75b9f
LeonKar <l.karagishev@gmail.com>
2022-02-28 12:14:11 +0100

    Menu

--- F1 Stats/src/Main.java
+++ F1 Stats/src/Main.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
 public class Main {
+	static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
 		System.out.println("Estadistica F1");
 		String login="", password="", check;
-		Scanner sc = new Scanner(System.in);
 		System.out.println("Introduzca su login:");
 		login = sc.next();
 		System.out.println("Introduzca su contraseña:");
@@ -15,6 +15,7 @@ public class Main {
 		}else {
 			menu(check);
 		}
+		sc.close();
 	}
 	
 	public static String login(String login, String password) {
@@ -30,11 +31,60 @@ public class Main {
 	}
 	
 	public static void menu(String check) {
-		System.out.println("Eliga opcion de menu:");
+		int option=0;
+		System.out.println("Eliga opcion de menu(introducir numero):");
 		if(check=="admin") {
-			
+			System.out.println("1)Option 1\n2)Option 2");
+		try {
+			option = sc.nextInt();
+			if((option>0)&&(option<3)) {
+				
+			}else {
+				System.out.println("No existe opcion");
+				System.out.println("Introduzca de nuevo:");
+				option = sc.nextInt();
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("No existe opcion");
+			System.out.println("Introduzca de nuevo:");
+			option = sc.nextInt();
+		}
+		switch(option) {
+			case 1:
+				System.out.println("Has elegido menu admin 1");
+			break;
+			case 2:
+				System.out.println("Has elegido menu admin 2");
+			break;
+		}
 		}else if(check=="user") {
-			
+			System.out.println("1)Option 1\n2)Option 2");
+			try {
+				option = sc.nextInt();
+				if((option>0)&&(option<3)) {
+					
+				}else {
+					System.out.println("No existe opcion");
+					System.out.println("Introduzca de nuevo:");
+					option = sc.nextInt();
+				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.out.println("No existe opcion");
+				System.out.println("Introduzca de nuevo:");
+				option = sc.nextInt();
+			}
+			switch(option) {
+			case 1:
+				System.out.println("Has elegido menu user 1");
+			break;
+			case 2:
+				System.out.println("Has elegido menu user 2");
+			break;
+			}
 		}
 	}
 }
