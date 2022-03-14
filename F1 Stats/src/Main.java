f95fae2b79771858c84889982057449186a11366
LeonKar <l.karagishev@gmail.com>
2022-03-14 10:09:49 +0100

    Perfect login
    Lee datos from json
    Cada usuario tiene role
    Cada tipo de usuario tiene su menu(entra automaticamente segun su role)

--- F1 Stats/src/Main.java
+++ F1 Stats/src/Main.java
@@ -1,8 +1,30 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Scanner;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+
+import model.Login;
+
 public class Main {
+	protected ArrayList<Login> loginList = new ArrayList<Login>();
 	static Scanner sc = new Scanner(System.in);
-	public static void main(String[] args) {
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+	public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
+
 		System.out.println("Estadistica F1");
 		String login="", password="", check;
 		System.out.println("Introduzca su login:");
@@ -18,23 +40,67 @@ public class Main {
 		sc.close();
 	}
 	
-	public static String login(String login, String password) {
+	public static String login(String login, String password) throws FileNotFoundException, IOException, ParseException{
 		String check="error";
-		if(login.equals("admin")&&password.equals("1234")) {
-			System.out.println("Hola admin");
-			check="admin";
-		}else if(login.equals("user1")&&password.equals("psswd1")){
-			System.out.println("Hola " + login);
-			check="user";
+		try {
+			JsonNode root = mapper.readTree(new File("userbase.json"));
+			JsonNode contactNode = root.path("users");
+            if (contactNode.isArray()) {
+                for (JsonNode node : contactNode) {
+                	String usr = node.path("Username").asText();
+                    String pswd = node.path("Password").asText();
+                    String rl = node.path("Role").asText();
+                    //System.out.println("Username : " + usr);
+                	if(login.equals(usr)&&password.equals(pswd)&&rl.equals("admin")) {
+            			System.out.println("Hola admin");
+            			check="admin";
+            		}else if(login.equals(usr)&&password.equals(pswd)&&rl.equals("user")){
+            			System.out.println("Hola user");
+            			check="user";
+            		}
+                }
+            }
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+
 		return check;
 	}
 	
 	public static void menu(String check) {
+		if(check=="admin") {
+			menuAdmin();
+		}else if(check=="user") {
+			menuUser();
+		}
+	}
+	public static void menuAdmin() {
 		int option=0;
 		System.out.println("Eliga opcion de menu(introducir numero):");
-		if(check=="admin") {
-			System.out.println("1)Option 1\n2)Option 2");
+		System.out.println("Admin menu:");
+		System.out.println("1)Option 1\n2)Option 2");
+		do {
+			option=sc.nextInt();
+		    while (!sc.hasNextInt()) {
+		        System.out.println("Introduzca un numero por favor");
+		        option = sc.nextInt();
+		    }
+		} while (sc.hasNextInt());
+	switch(option) {
+		case 1:
+			System.out.println("Has elegido menu admin 1");
+		break;
+		case 2:
+			System.out.println("Has elegido menu admin 2");
+		break;
+	}
+	}
+	public static void menuUser() {
+		int option=0;
+		System.out.println("Eliga opcion de menu(introducir numero):");
+		System.out.println("User menu");
+		System.out.println("1)Option 1\n2)Option 2");
 		try {
 			option = sc.nextInt();
 			if((option<1)||(option>3)) {
@@ -52,39 +118,12 @@ public class Main {
 			option = sc.nextInt();
 		}
 		switch(option) {
-			case 1:
-				System.out.println("Has elegido menu admin 1");
-			break;
-			case 2:
-				System.out.println("Has elegido menu admin 2");
-			break;
-		}
-		}else if(check=="user") {
-			System.out.println("1)Option 1\n2)Option 2");
-			try {
-				option = sc.nextInt();
-				if((option<1)||(option>3)) {
-					System.out.println("No existe opcion");
-					System.out.println("Introduzca de nuevo:");
-					option = sc.nextInt();
-				}else {
-					
-				}
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				System.out.println("No existe opcion");
-				System.out.println("Introduzca de nuevo:");
-				option = sc.nextInt();
-			}
-			switch(option) {
-			case 1:
-				System.out.println("Has elegido menu user 1");
-			break;
-			case 2:
-				System.out.println("Has elegido menu user 2");
-			break;
-			}
+		case 1:
+			System.out.println("Has elegido menu user 1");
+		break;
+		case 2:
+			System.out.println("Has elegido menu user 2");
+		break;
 		}
 	}
 }
