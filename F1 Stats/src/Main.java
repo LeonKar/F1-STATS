c7cf68030e2b35df2a8353266e959950dab7e71d
LeonKar <l.karagishev@gmail.com>
2022-03-21 11:40:00 +0100

    Cambios login y menu
    He añadido puntos de ambos menu, modificado fichero pilotos. Actualizado login con registro

--- F1 Stats/src/Main.java
+++ F1 Stats/src/Main.java
@@ -15,6 +15,8 @@ import org.json.simple.parser.ParseException;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.gson.Gson;
 
 import model.Login;
@@ -25,27 +27,61 @@ public class Main {
     private static final ObjectMapper mapper = new ObjectMapper();
 
 	public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
-
-		System.out.println("Estadistica F1");
-		String login="", password="", check;
-		System.out.println("Introduzca su login:");
-		login = sc.next();
-		System.out.println("Introduzca su contraseña:");
-		password = sc.next();
-		check = login(login, password);
-		if(check.equals("error")) {
-			System.out.println("Login o contraseña incorrecto");
-		}else {
-			menu(check);
-		}
-		sc.close();
+		int option=1;
+		boolean cont=false;
+		do {
+			System.out.println("Estadistica F1");
+			System.out.println("1. Regisrarse");
+			System.out.println("2. Login");
+            try {
+                System.out.println("Escribe una de las opciones");
+                option = sc.nextInt();
+                switch (option) {
+                    case 1:
+                        System.out.println("Registro en proceso");
+                        cont=true;
+                        break;
+                    case 2:
+                    	String login="", password="", check;
+                		do {
+                		System.out.println("Estadistica F1");
+                		System.out.println("Introduzca su login:");
+                		login = sc.next();
+                		System.out.println("Introduzca su contraseña:");
+                		password = sc.next();
+                		
+                			check = login(login, password);
+                			if(check.equals("error")) {
+                				System.out.println("Login o contraseña incorrecto\n");
+                			}else {
+                				menu(check);
+                			}
+                		}while(check=="error");
+                		sc.close();
+                        cont=true;
+                        break;
+                    default:
+                        System.out.println("Solo números entre 1 y 2");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Debes insertar un número");
+                sc.next();
+            }
+        }while(cont!=true);	
 	}
 	
 	public static String login(String login, String password) throws FileNotFoundException, IOException, ParseException{
 		String check="error";
 		try {
+			
 			JsonNode root = mapper.readTree(new File("userbase.json"));
 			JsonNode contactNode = root.path("users");
+			
+			/*Login lgn = new Login("1", "2", "3");
+			ObjectMapper mapper = new ObjectMapper();
+			mapper.writeValue(Paths.get("userbase.json").toFile(), lgn);
+			 */
+	        
             if (contactNode.isArray()) {
                 for (JsonNode node : contactNode) {
                 	String usr = node.path("Username").asText();
@@ -79,12 +115,12 @@ public class Main {
 	public static void menuAdmin() {
 		int option=1;
 		boolean cont=false;
-		System.out.println("Eliga opcion de menu(introducir numero):");
-		System.out.println("Admin menu:");
+		System.out.println("Menu\nEliga opcion de menu(introducir numero):");
 		do {
-            System.out.println("1. Opcion 1");
-            System.out.println("2. Opcion 2");
-            System.out.println("3. Opcion 3");
+            System.out.println("1. Eliminar usuario");
+            System.out.println("2. Modificar estadistica");
+            System.out.println("3. Añadir estadistica");
+            System.out.println("4. Añadir usuario admin");
  
             try {
  
@@ -116,13 +152,22 @@ public class Main {
 	public static void menuUser() {
 		int option=1;
 		boolean cont=false;
-		System.out.println("Eliga opcion de menu(introducir numero):");
-		System.out.println("User menu");
+		System.out.println("Clasificación:");
+		
+		System.out.println("Proxima carrera: ");
+		
+		System.out.println("\nMenu\nEliga opcion de menu(introducir numero):");
 		do {
-            System.out.println("1. Lista de pilotos");
-            System.out.println("2. Opcion 2");
-            System.out.println("3. Opcion 3");
- 
+			System.out.println("1. Editar el perfil de usuario");
+            System.out.println("2. Ver lista de pilotos");
+            System.out.println("3. Ver lista de carreras GP");
+            System.out.println("4. Ver calendario");
+            System.out.println("5. Clasificación del campeonato de constructores");
+            System.out.println("6. Ver caracteristicas tecnicas de los coches");
+            System.out.println("7. Mejores tiempos por circuito de cada piloto");
+            System.out.println("8. Ultimos resultados");
+            System.out.println("9. Comparativa entre pilotos");
+            System.out.println("10. Simulación de puntos");
             try {
  
                 System.out.println("Escribe una de las opciones");
@@ -130,13 +175,14 @@ public class Main {
  
                 switch (option) {
                     case 1:
-                        verPilotos();
+                    	System.out.println("Has seleccionado la opcion 1");
                         cont=true;
                         break;
                     case 2:
-                        System.out.println("Has seleccionado la opcion 2");
+                    	verPilotos();
                         cont=true;
                         break;
+                        
                     case 3:
                         System.out.println("Has seleccionado la opcion 3");
                         cont=true;
@@ -153,6 +199,7 @@ public class Main {
 
 	private static void verPilotos() {
 		// TODO Auto-generated method stub
+		System.out.println("\nPilotos de la Fórmula 1:\n");
 		try {
 			JsonNode root = mapper.readTree(new File("pilotos.json"));
 			JsonNode contactNode = root.path("pilotos");
@@ -161,10 +208,11 @@ public class Main {
                 	String numero = node.path("numero").asText();
                     String nombre = node.path("nombre").asText();
                     String equipo = node.path("equipo").asText();
-                    String chasis = node.path("chasis").asText();
-                    String motor = node.path("motor").asText();
-                    System.out.println("Numero: " + numero + ", nombre: " + nombre + ", equipo: " +
-                    equipo + ", chasis: " + chasis + ", motor: " + motor);
+                    String nacionalidad = node.path("nacionalidad").asText();
+                    String edad = node.path("edad").asText();
+                    String campeonatos_ganados = node.path("campeonatos_ganados").asText();
+                    System.out.println("\tNumero: " + numero + ", nombre: " + nombre + ", equipo: " +
+                    equipo + ", nacionalidad: " + nacionalidad + ", edad: " + edad + ", campeonatos ganados: " + campeonatos_ganados);
                 	
                 }
             }
